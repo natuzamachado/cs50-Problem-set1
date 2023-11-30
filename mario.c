170f9401f7f9790fb4b3baead2c462129809102b
Natuza Machado <63171826+natuzamachado@users.noreply.github.com>
2023-11-30 20:42:31 -0300

    Update mario.c

--- mario.c
+++ mario.c
@@ -2,15 +2,18 @@
 #include <cs50.h>
 
 int main(void)
-{
-    //get positive integer from user between 1 and 8, both inclusive.
+{  
     int height;
+    
+     //get positive integer from user between 1 and 8, both inclusive.
+
     do 
     {
         height = get_int("Height: ");
     }
     while( height < 1 || height > 8);
 
+   
     //display a pyramid as taller as the height.
    
    for(int row = 0; row < height; row++)
