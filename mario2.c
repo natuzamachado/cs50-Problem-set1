7f75f4eaa09a4130eaa45b412a52b2b9ce0bc69d
Natuza Machado <63171826+natuzamachado@users.noreply.github.com>
2023-11-30 20:59:47 -0300

    Add files via upload

---
+++ mario2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // get positive integer from user between 1 and 8, both inclusive.
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+
+    } while (height < 1 || height > 8);
+
+    // display a pyramid as taller as the height.
+
+    for (int row = 1; row <= height; row++)
+    {
+        char brick = '#';
+        char space = ' ';
+
+        for(int i = 0; i < height - row; i++){
+            printf("%c", space);
+        }
+
+        for (int col = 1; col <= row ; col++)
+        {
+            printf("%c", brick);
+        }
+        printf("\n");
+    }
+}
